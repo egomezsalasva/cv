03f89ccaeafa87178d11ba0c082d7a5c5b4d8ce4
Kike <kike@Kikes-MacBook-Pro.local>
2020-06-30 13:07:03 +0200

    Summary Button

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,35 @@ import Header from './components/global/Header'
 //STYLE
 //-Components
 const NavContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  margin: 30px auto;
+  padding: 0 70px;
+  font-family: 'SpaceMono-Regular';
+  font-size: 14px;
+  color: #000;
+  text-align: center;
+  text-transform: lowercase;
+  .activeNav{
+    font-weight: 700;
+  }
 `
+const SummaryButtonContainer = styled.div`
+  margin-top: 30px;
+  text-align: center;
+  .summaryButton{
+    display: inline-block;
+    padding: 14px 20px 10px;
+    border: 1px solid #000;
+    font-family: 'Graphik-Regular';
+    font-size: 14px;
+    color: #000;
+    text-align: center;
+    cursor: pointer;
+  }
+`
+
 
 function App() {
   return (
@@ -16,8 +44,14 @@ function App() {
       <Header/>
 
       <NavContainer>
-
+        <div className="activeNav">C.V.</div>
+        <div>Portfolio</div>
+        <div>Library</div>
       </NavContainer>
+
+      <SummaryButtonContainer>
+        <div className="summaryButton">Don’t Make Me Read All This Bollocks</div>
+      </SummaryButtonContainer>
       
       
     </>
