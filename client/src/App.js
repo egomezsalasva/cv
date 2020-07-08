d71c666ec903fbfe77ee681e5bc676083dd5643c
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-08 14:05:47 +0200

    Portfolio Styling and import images

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@
 //-Modules
 import React from 'react'
 // import styled from 'styled-components'
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom"
 
 //-Components
 import Header from './components/global/Header'
--- client/src/components/portfolio/Portfolio.js
+++ client/src/components/portfolio/Portfolio.js
@@ -4,18 +4,40 @@ import React from 'react'
 import styled from 'styled-components'
 //-Components
 import Heading from '../cv/Heading'
+//-Images
+import scrannysHouseImg from '../../images/scrannysHouse.png'
+import apollo30Img from '../../images/apollo30.png'
 
+//DATA
+const portfolioData = [
+  {
+    name: "Scranny's House",
+    imgUrl: scrannysHouseImg,
+    webUrl: "https://www.scrannyshouse.com/",
+  },
+  {
+    name: "Apollo 30",
+    imgUrl:apollo30Img,
+    webUrl: "https://apollo30.com/",
+  }
+]
 
 //STYLE
 //-Components
 const SectionsOuter = styled.div`
   width: 100%;
-  height: calc(100vh - 210px - 60px);
+  height: calc(100vh - 210px );
   margin-top: 30px;
   overflow: scroll;
   .section{
     margin: 0 30px 50px;
     max-width: 650px;
+    .portfolioImg{
+      width: 100%;
+      height: 200px;
+      background-repeat: no-repeat;
+
+    }
   }
 `
 
@@ -26,14 +48,22 @@ function Portfolio() {
     <>
       <SectionsOuter>
         <div className="sectionsInner">
-          <div className="section">
-            <Heading headingName="Scranny's House"/>
-            {/* <img src="" alt="" /> */}
-          </div>
-          <div className="section">
-            <Heading headingName="Apollo 30"/>
-            {/* <img src="" alt="" /> */}
-          </div>
+          {portfolioData.map( section => {
+            return (
+                      <a href={section.webUrl} target="_blank" rel="noopener noreferrer">
+                        <div className="section">
+                          <Heading headingName={section.name}/>
+                          <div className="portfolioImg" 
+                            style={{ 
+                              background: `url(${section.imgUrl}) center center`,
+                              backgroundSize: "cover",
+                              backgroundRepeat: "no-repeat",
+                            }}
+                          />
+                        </div>
+                      </a>
+                   )
+          })}
         </div>
       </SectionsOuter>
     </>     
