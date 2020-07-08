f634718de6092ded037c95c8a8790cd85657f40f
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-08 13:39:51 +0200

    Portfolio Structure

--- client/src/components/portfolio/Portfolio.js
+++ client/src/components/portfolio/Portfolio.js
@@ -1,20 +1,41 @@
 //IMPORTS
 //-Modules
 import React from 'react'
-// import styled from 'styled-components'
+import styled from 'styled-components'
+//-Components
+import Heading from '../cv/Heading'
 
 
 //STYLE
 //-Components
-// const Component = styled.div`
-  
-// `
+const SectionsOuter = styled.div`
+  width: 100%;
+  height: calc(100vh - 210px - 60px);
+  margin-top: 30px;
+  overflow: scroll;
+  .section{
+    margin: 0 30px 50px;
+    max-width: 650px;
+  }
+`
 
 
 //MAIN COMPONENT
 function Portfolio() {
   return (
     <>
+      <SectionsOuter>
+        <div className="sectionsInner">
+          <div className="section">
+            <Heading headingName="Scranny's House"/>
+            {/* <img src="" alt="" /> */}
+          </div>
+          <div className="section">
+            <Heading headingName="Apollo 30"/>
+            {/* <img src="" alt="" /> */}
+          </div>
+        </div>
+      </SectionsOuter>
     </>     
   )
 }
