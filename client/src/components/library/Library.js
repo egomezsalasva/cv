dc58d2d88c5fa446c3bc6fe01ede86af1a49b18d
Kike <kike@Kikes-MacBook-Pro.local>
2020-07-09 19:14:46 +0200

    Book inputs

--- client/src/components/library/Library.js
+++ client/src/components/library/Library.js
@@ -130,12 +130,114 @@ const Library = () => {
           </div>
 
           <div className="section">
-            <Heading headingName="Design" classProp="heading" />
+            <Heading headingName="Behavioural Science" classProp="heading" />
             <ul>
               <li>
-                <span className="bookTitle">100 Ideas That Changed Graphic Design</span>
+                <span className="bookTitle">Why We Sleep</span>
+                <span className="bookSubtitle">, The new science of sleep and dreams, </span>
+                <span className="bookAuthor">Matthew Walker</span>
+              </li>
+              <li>
+                <span className="bookTitle">Drive</span>
+                <span className="bookSubtitle">, The Surprising Truth About What Motivates Us, </span>
+                <span className="bookAuthor">Daniel H. Pink</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Science of Fear</span>
+                <span className="bookSubtitle">, How the culture of fear manipulates your brain, </span>
+                <span className="bookAuthor">Daniel Gardner</span>
+              </li>
+              <li>
+                <span className="bookTitle">Algorithms To Live By</span>
+                <span className="bookSubtitle">, The computer science of human decisions, </span>
+                <span className="bookAuthor">Brian Christian</span>
+                <span className="bookSubtitle"> and </span>
+                <span className="bookAuthor">Tom Griffiths</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Art Of Learning</span>
+                <span className="bookSubtitle">, An inner journey to optimal performance, </span>
+                <span className="bookAuthor">Josh Waitzkin</span>
+              </li>
+              <li>
+                <span className="bookTitle">Never Eat Alone</span>
+                <span className="bookSubtitle">, And other secrets to success, one relationship at a time, </span>
+                <span className="bookAuthor">Keith Ferrazzi</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Tipping Point</span>
+                <span className="bookSubtitle">, How little things can make a big difference, </span>
+                <span className="bookAuthor">Malcolm Gladwell</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Power Of Habit</span>
+                <span className="bookSubtitle">, Why we do what we do, and how to change, </span>
+                <span className="bookAuthor">Charles Duhigg</span>
+              </li>
+            </ul>
+          </div>
+
+          <div className="section">
+            <Heading headingName="Business" classProp="heading" />
+            <ul>
+              <li>
+                <span className="bookTitle">Mastering The VC Game</span>
+                <span className="bookSubtitle">, A venture capital insider reveals how to get from start-up to IPO on your terms, </span>
+                <span className="bookAuthor">Jeffrey Bussgang</span>
+              </li>
+              <li>
+                <span className="bookTitle">Rich Dad, Poor Dad</span>
+                <span className="bookSubtitle">, What the rich teach their kids about money that the poor and middle class do not, </span>
+                <span className="bookAuthor">Robert T. Kiyosaki</span>
+              </li>
+              <li>
+                <span className="bookTitle">Start with Why</span>
+                <span className="bookSubtitle">, How great leaders inspire everyone to take action, </span>
+                <span className="bookAuthor">Simon Sinek</span>
+              </li>
+              <li>
+                <span className="bookTitle">How Google Works</span>
                 <span className="bookSubtitle">, </span>
-                <span className="bookAuthor">Steven Heller</span>
+                <span className="bookAuthor">Eric Schmidt</span>
+                <span className="bookSubtitle"> and </span>
+                <span className="bookAuthor">Jonathan Rosenberg</span>
+              </li>
+              <li>
+                <span className="bookTitle">The One Minute Manager</span>
+                <span className="bookSubtitle">, </span>
+                <span className="bookAuthor">Kenneth Blanchard</span>
+                <span className="bookSubtitle"> and </span>
+                <span className="bookAuthor">Spencer Johnson</span>
+              </li>
+              <li>
+                <span className="bookTitle">Zero To One</span>
+                <span className="bookSubtitle">, Notes on startups, or how to build the future, </span>
+                <span className="bookAuthor">Peter Thiel</span>
+              </li>
+              <li>
+                <span className="bookTitle">The 4-Hour Workweek</span>
+                <span className="bookSubtitle">, Escape 9-5, live anywhere, and join the new rich, </span>
+                <span className="bookAuthor">Timothy Ferriss</span>
+              </li>
+              <li>
+                <span className="bookTitle">The $100 Startup</span>
+                <span className="bookSubtitle">, Reinvent the way you make a living, do what you love, and create a new future, </span>
+                <span className="bookAuthor">Chris Guillebeau</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Lean Startup</span>
+                <span className="bookSubtitle">, How today's entrepreneurs use continuous innovation to create radically successful businesses, </span>
+                <span className="bookAuthor">Eric Ries</span>
+              </li>
+              <li>
+                <span className="bookTitle">The Innovator's Dilemma</span>
+                <span className="bookSubtitle">, When new technologies cause great firms to fail, </span>
+                <span className="bookAuthor">Clayton M. Christensen</span>
+              </li>
+              <li>
+                <span className="bookTitle">Getting to Yes</span>
+                <span className="bookSubtitle">, Negotiating agreement without giving in, </span>
+                <span className="bookAuthor">Roger Fisher</span>
               </li>
             </ul>
           </div>
